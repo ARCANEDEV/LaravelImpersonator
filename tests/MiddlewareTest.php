f6abe5378523eb79e3f4f7727cc6af048b3e23b3
ARCANEDEV <arcanedev.maroc@gmail.com>
2017-05-10 16:11:25 +0100

    Adding a middleware to protect the routes from impersonations

---
+++ tests/MiddlewareTest.php
@@ -0,0 +1,39 @@
+<?php namespace Arcanedev\LaravelImpersonator\Tests;
+
+/**
+ * Class     MiddlewareTest
+ *
+ * @package  Arcanedev\LaravelImpersonator\Tests
+ * @author   ARCANEDEV <arcanedev.maroc@gmail.com>
+ */
+class MiddlewareTest extends TestCase
+{
+    /* -----------------------------------------------------------------
+     |  Tests
+     | -----------------------------------------------------------------
+     */
+
+    /** @test */
+    public function it_can_block_the_access_to_secured_routes_while_impersonating()
+    {
+        $this->loginWithId(1);
+
+        $response = $this->get(route('admin::dashboard'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Dashboard page');
+
+        $this->get(route('auth::impersonator.start', [2]));
+
+        $response = $this->get(route('admin::dashboard'));
+
+        $response->assertStatus(302);
+
+        $this->get(route('auth::impersonator.stop'));
+
+        $response = $this->get(route('admin::dashboard'));
+
+        $response->assertSuccessful();
+        $response->assertSee('Dashboard page');
+    }
+}
